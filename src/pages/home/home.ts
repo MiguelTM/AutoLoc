4aae7cd46478c1835ea17eee7ea7c0bf27b9efff
MiguelTM <matm1488@gmail.com>
2017-10-22 20:31:51 -0500

    up

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
+
+declare var google:any;
 
 @Component({
   selector: 'page-home',
@@ -7,7 +9,42 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  @ViewChild('map') mapRef: ElementRef;
+
   constructor(public navCtrl: NavController) {
+
+  }
+
+  ionViewDidLoad() {
+
+    console.log(this.mapRef);
+    this.DisplayMap();
+  }
+
+  DisplayMap() {
+
+    const location = new google.maps.LatLng("21.019015", "-101.257359");
+
+    const options = {
+
+      center: location,
+      zoom: 10,
+      streetViewControl:false,
+      mapTypeId:'hybrid'
+    };
+
+    const map = new google.maps.Map(this.mapRef.nativeElement, options);
+this.addMarker(location,map);
+
+  }
+
+  addMarker(position,map){
+
+return new google.maps.Marker({
+
+position,map
+
+});
 
   }
 
